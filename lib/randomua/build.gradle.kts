eef4deb784f99383c1e141fa05c5c03ad7d80326
w <wvvwvvvvwvvw@gmail.com>
2026-03-26 02:39:25 -0700

    delete

--- lib/randomua/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-plugins {
-    id("lib-android")
-}
-
-dependencies {
-    compileOnly("com.squareup.okhttp3:okhttp-brotli:5.3.2")
-}
--- lib/zipinterceptor/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-plugins {
-    id("lib-android")
-}
-
-dependencies {
-    compileOnly("com.github.tachiyomiorg:image-decoder:e08e9be535")
-}
